4e8cbf47bdf77d0fc770cb6efe77616beef797d2
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-08 16:45:44 +0900

    ヘッダー作成

--- resources/views/toppages/index.blade.php
+++ resources/views/toppages/index.blade.php
@@ -3,30 +3,41 @@
   <head>
     <!-- 以下の meta tags（charset と viewport）は必須です-->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap の CSS（CDN経由）の読み込み -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- <link href="{{asset("css/main_page.css")}}" rel="stylesheet"> -->
-    
-    <title>Dream Diary</title>
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300 rel="stylesheet">
     <link href="css/_reset.scss" rel="stylesheet" type="text/css">
-    <link href="css/mainpage.scss" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/mainpage.css') }}" rel="stylesheet">
+    <title>Dream Diary</title>
+    
   </head>
   <body>
     <header>
-      <h2>Dream diary</h2>
-      @if( Auth::check() )
-        <a href="{!! action('TopController@index') !!}">Home</a>
-        <div class = "use_name_box">
-          {{$user->name}}
+      <div class="head_content">
+        <div class="title">
+          <a href="{!! action('TopController@index') !!}">
+            <div class="text_font">
+              Dream diary
+            </div>
+          </a>
         </div>
-        <a href="/users/{{$user->id}}">{{$user->name}}さんのマイページ</a><br />
-        <a href="{{ route('logout') }}">ログアウト</a>
-      @else
-        <a href="{{ route('login') }}">ログイン</a>
-        <a href="{{ route('register') }}">新規登録</a>
-      @endif
+          @if( Auth::check() )
+            <div class = "par_button">
+              <div class = "user_name_box">
+                <a href="/users/{{$user->id}}">{{$user->name}}さんのマイページ</a>
+              </div>
+              <div class = "logout_box">
+                <a href="{{ route('logout') }}">ログアウト</a>
+              </div>
+            </div>
+            
+          @else
+            <a href="{{ route('login') }}">ログイン</a>
+            <a href="{{ route('register') }}">新規登録</a>
+          @endif
+        </div>
     </header>
+    <div class="main_page">
+    </div>
   </body>
 </html>
