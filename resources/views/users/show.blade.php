26964876fc6ba920fc2fab2529650e5a0e924e4e
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-09 18:58:34 +0900

    ユーザー詳細ページ編集

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -37,32 +37,46 @@
             </div>
         </div>
     </header>
-      <div class="main_box">
-        <div class="use_content_box">
-          <div class="per_box">
-            <div class="form_box">
-              <a href="/users/{{$user->id}}/contents" method="get">新規投稿</a><br />
+      <div class="main_page">
+        <div class="main_box">
+          <div class="use_content_box">
+            <div class="per_box">
+              <div class="per_img_box">
+                <div class="per_img_box_box">
+                  @if($user->icon != null)
+                    <img src="/public/avatar/{{$user->icon}}" alt="inu" class="icon_image" width="90" height="90">
+                  @else
+                    <img src="/images/azarasi.png" alt="inu" class="icon_image" width="90" height="90">
+                  @endif
+                </div>
+              </div>
+              <div class="per_name">
+                {{$user->name}} さん
+              </div>
+              <div class="form_box">
+                <a href="/users/{{$user->id}}/contents" method="get">新規投稿</a><br />
+              </div>
             </div>
-          </div>
-          <div class="content_box">
-            @foreach ($contents as $content)
-              <div class="content_title">
-                {{$content->title}}
-              </div>
-              <div class="content_body">
-                {{$content->body}}
-              </div>
-              <div class="content_edit_button">
-                <a href="/users/{{$user->id}}/contents/{{$content->id}}/edit" method="get">編集</a><br />
-              </div>
-              <div class="content_delete_button">
-              <form action="/users/{{$user->id}}/contents/{{$content->id}}" method="post">
-                {{ csrf_field() }}
-                <input name="_method" type="hidden" value="DELETE">
-                <input type="submit" class="delete" value="削除">
-              </form>
-              </div>
-            @endforeach
+            <div class="content_box">
+              @foreach ($contents as $content)
+                <div class="content_title">
+                  {{$content->title}}
+                </div>
+                <div class="content_body">
+                  {{$content->body}}
+                </div>
+                <div class="content_edit_button">
+                  <a href="/users/{{$user->id}}/contents/{{$content->id}}/edit" method="get">編集</a><br />
+                </div>
+                <div class="content_delete_button">
+                <form action="/users/{{$user->id}}/contents/{{$content->id}}" method="post">
+                  {{ csrf_field() }}
+                  <input name="_method" type="hidden" value="DELETE">
+                  <input type="submit" class="delete" value="削除">
+                </form>
+                </div>
+              @endforeach
+            </div>
           </div>
         </div>
       </div>
