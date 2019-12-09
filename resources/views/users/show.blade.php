b166b4601b061eb3ddd2e86e7f8095d3ecdb28b6
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-10 01:54:29 +0900

    編集と削除ボタン変更

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -41,7 +41,7 @@
             </div>
         </div>
     </header>
-      <div class="main_page">
+      <div class="main_page_per">
         <div class="main_box">
           <div class="use_content_box">
             <div class="per_box">
@@ -97,22 +97,30 @@
                 </div>
                 <div class="main_zone">
                   @foreach ($contents as $content)
-                    <div class="content_day">
-                      投稿日
-                      {{$content->created_at}}
-                    </div>
-                    <div class="content_title">
-                      タイトル:
-                      {{$content->title}}
-                    </div>
-                    <div class="content_body">
-                      {{$content->body}}
-                    </div>
-                    <div class="content_edit_button">
-                      <a href="/users/{{$user->id}}/contents/{{$content->id}}/edit" method="get">編集</a><br />
-                    </div>
-                    <div class="content_delete_button">
-                      <a href="/users/{{$user->id}}/contents/{{$content->id}}/delete" method="get">削除</a><br />
+                    <div class="content_main_box">
+                      <div class="content_day">
+                        投稿日
+                        {{$content->created_at}}
+                      </div>
+                      <div class="content_title">
+                        タイトル:
+                        {{$content->title}}
+                      </div>
+                      <div class="content_body">
+                        {{$content->body}}
+                      </div>
+                      <div class="content_edit_button">
+                        <a href="/users/{{$user->id}}/contents/{{$content->id}}/edit" method="get">
+                          <img src="/images/illust1109.png" alt="inu" class="content_edit_image" width="45" height="45">
+                          <div class="edit_content">編集</div>
+                        </a><br />
+                      </div>
+                      <div class="content_delete_button">
+                        <a href="/users/{{$user->id}}/contents/{{$content->id}}/delete" method="get">
+                          <img src="/images/namagomi.png" alt="inu" class="content_edit_image" width="50" height="45">
+                          <div class="edit_content">削除</div>
+                        </a><br />
+                      </div>
                     </div>
                   @endforeach
                 </div>
