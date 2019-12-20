ef89d560000ed2bc9c669be6d2c9d5fa61e9613d
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-20 21:16:52 +0900

    ログイン画面マークアップ

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,16 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+        <div class="col-md-8 col-md-offset-2" id = "all_login_box">
+            <div class="login_box">
                 <div class="panel-heading">Login</div>
-
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}" id = "form_input">
+                            <label for="email" class="col-md-4 control-label" id="email_box">メールアドレスを入力してください</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -25,8 +24,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}" id = "pass_input">
+                            <label for="password" class="col-md-4 control-label" id="pass_box">パスワードを入力してください</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -39,24 +38,25 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group" id="rem_input">
                             <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                                <div class="checkbox">        
+                                    <input type="checkbox" id ="check_box1" name="remember" {{ old('remember') ? 'checked' : '' }}> 
+                                    <div class="check_box_text">
+                                        パスワード記憶する
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-8 col-md-offset-4" id = "login_btn">
+                                <button type="submit" class="btn btn-primary" id = "submit_btn">
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                <a class="btn btn-link" href="{{ route('password.request') }}" id = "forget_pass">
+                                    パスワードを忘れちゃった！！
                                 </a>
                             </div>
                         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,39 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300 rel="stylesheet">
+    <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mainpage.css') }}" rel="stylesheet">
+    <link href="css/_reset.css" rel="stylesheet" type="text/css">
 
     <title>Dream Diary</title>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        @guest
-                            <a href="{{ route('login') }}">ろぐいん</a>
-                            <a href="{{ route('register') }}">しんきとうろく</a>
-                        @else
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </ul>
-                        @endguest
-                    </ul>
-                </div>
+    <header>
+      <div class="head_content">
+        <div class="title">
+          <a href="{!! action('TopController@index') !!}">
+            <div class="text_font">
+              Dream diary
             </div>
-        </nav>
-
+          </a>
+        </div>
+        @if( Auth::check() )
+            <div class = "par_button">
+              <div class = "user_name_box">
+                <a href="/users/{{$user->id}}">{{$user->name}}さんのマイページ</a>
+              </div>
+              <div class = "logout_box">
+                <a href="{{ route('logout') }}"onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                  ログアウト
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+              </div>
+            </div>
+            
+          @else
+          <div class = "par_button">
+            <div class = "user_name_box">
+              <a href="{{ route('login') }}">ログイン</a>
+            </div>
+            <div class = "logout_box">
+              <a href="{{ route('register') }}">新規登録</a>
+            </div>
+          </div>
+          @endif
+        </div>
+    </header>
+    <div id="app">
         @yield('content')
     </div>
 
