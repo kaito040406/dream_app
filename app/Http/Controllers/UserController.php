a6426659bee9970cba2dea35f2a7618b3424e399
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-21 14:32:53 +0900

    修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use App\user;
-use App\content;
+use App\User;
+use App\Content;
 use Validator;
 
 class UserController extends Controller
