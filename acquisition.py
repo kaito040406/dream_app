ab298d40ed7e0d0021d315cdc0b8732e577e5b48
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-23 17:23:30 +0900

    スクレイピングテスト３

--- acquisition.py
+++ acquisition.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
  
-# r = requests.get('https://spicomi.net/media/articles/1540')
-r = requests.get("https://news.yahoo.co.jp/")
+r = requests.get('https://spicomi.net/media/articles/1540')
+# r = requests.get("https://news.yahoo.co.jp/")
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-print(soup.find("ul", "newsFeed_list").text)
+
+
+# print(data.find("content_ln").text)
+
+datas = soup.find_all("a")
+for data in datas:
+  print(data)
  
