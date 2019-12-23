6d292989a7b447edf04af8d483144ef7603ba021
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-23 18:10:19 +0900

    修正

--- acquisition.py
+++ acquisition.py
@@ -3,14 +3,13 @@ from bs4 import BeautifulSoup
  
 r = requests.get('https://spicomi.net/media/articles/1540')
 # r = requests.get("https://news.yahoo.co.jp/")
-
 soup = BeautifulSoup(r.content, "html.parser")
-
-
-
 # print(data.find("content_ln").text)
-
-datas = soup.find_all("a")
+datas = soup.select(".content_ln", recursive=False)
 for data in datas:
-  print(data)
+  data2 = data.select("a")
+  if len(data2) != 0:
+    print(data2)
+
+
  
