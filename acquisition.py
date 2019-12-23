d5ce18d7cf061041eb6ef9bd9e56a836ff0c61a6
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-23 19:45:42 +0900

    詳細データ取得

--- acquisition.py
+++ acquisition.py
@@ -1,18 +1,37 @@
 import requests
+import time
 from bs4 import BeautifulSoup
  
+def Detail_page(url):
+  print(url)
+  d_r = requests.get(url)
+  d_soup = BeautifulSoup(d_r.content, "html.parser")
+  d_datas_title = d_soup.select(".section content_ln header_ln header2_ln", recursive=False)
+  d_datas_text = d_soup.select(".content_ln", recursive=False)
+
+  for d_data_title in d_datas_title:
+    title = d_data_title.text
+    print(title)
+
+  for d_data_text in d_datas_text:
+    text = d_data_text.text
+    print(text)
+
+
+
 r = requests.get('https://spicomi.net/media/articles/1540')
-# r = requests.get("https://news.yahoo.co.jp/")
 soup = BeautifulSoup(r.content, "html.parser")
-# print(data.find("content_ln").text)
 datas = soup.select(".content_ln", recursive=False)
 for data in datas:
   datas2 = data.select("a")
   if len(datas2) != 0:
-    # for data3 in data2:
-    # print(data2.get("href"))
     for data2 in datas2:
-      url = data2.get("href")
+      # url = data2.get("href")
+      # print(data2.get("href"))
+      Detail_page(data2.get("href"))
+      time.sleep(60)
+
+
 
 
  
