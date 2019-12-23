296343023280bcd7348e363a47e0a5c640ef7872
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-23 22:44:09 +0900

    データ抽出部分修正

--- acquisition.py
+++ acquisition.py
@@ -6,18 +6,17 @@ def Detail_page(url):
   print(url)
   d_r = requests.get(url)
   d_soup = BeautifulSoup(d_r.content, "html.parser")
-  d_datas_title = d_soup.select(".section content_ln header_ln header2_ln", recursive=False)
   d_datas_text = d_soup.select(".content_ln", recursive=False)
-
-  for d_data_title in d_datas_title:
-    title = d_data_title.text
-    print(title)
+  last = int(len(d_datas_text)) - 3
+  k = 0
 
   for d_data_text in d_datas_text:
-    text = d_data_text.text
-    print(text)
-
-
+    if  k >= 3 and k <= last:
+      text = d_data_text.text
+      print(k)
+      print(text)
+    k = k + 1
+    
 
 r = requests.get('https://spicomi.net/media/articles/1540')
 soup = BeautifulSoup(r.content, "html.parser")
@@ -26,10 +25,8 @@ for data in datas:
   datas2 = data.select("a")
   if len(datas2) != 0:
     for data2 in datas2:
-      # url = data2.get("href")
-      # print(data2.get("href"))
       Detail_page(data2.get("href"))
-      time.sleep(60)
+      time.sleep(5)
 
 
 
