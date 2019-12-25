086cba9ec9f3945096da16def6ea29c0e32b6122
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-26 00:20:52 +0900

    修正

--- python_file/cal.py
+++ python_file/cal.py
@@ -1,9 +1,15 @@
 import csv
 import pprint
 import time
+import re
+
 
 with open('uranai.csv') as f:
   reader = csv.reader(f)
   for row in reader:
-    print(row)
+    katakana_word = re.findall("[ァ-ン]", row[1])
+    kanji_word = re.findall("[一-龥]", row[1])
+    print(row[0])
+    print(katakana_word)
+    print(kanji_word)
     time.sleep(1)
