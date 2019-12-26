e710dadf16bcf5eb6c81b8474ebfd4ff74dcb4ee
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-26 17:37:36 +0900

    ポジティブネガティブ判定

--- python_file/cal.py
+++ python_file/cal.py
@@ -3,15 +3,31 @@ import pprint
 import time
 import re
 
-
 with open('uranai.csv') as f:
   reader = csv.reader(f)
   for row in reader:
-    # katakana_word = re.findall("[ァ-ン]", row[1])
-    # kanji_word = re.findall("[一-龥]", row[1])
-    # print(row[0])
-    # print(katakana_word)
-    # print(kanji_word)
-
-    
-    time.sleep(1)
+    print(row[1])
+    row[2] = 0
+    with open('changed_data.csv') as c:
+      words = csv.reader(c)
+      i = 0
+      for word in words:
+        if word[0] in row[1]:
+          if word[1] == 'p':
+            row[2] = row[2] + 1
+            print('+')
+          elif word[1] == 'n':
+            row[2] = row[2] - 1
+            print('-')
+          else:
+            print('変動なし')
+        else:
+          print('該当ワードなし')
+        print('STEP'+str(i))
+        i = i +1
+        # time.sleep(0.02)
+      c.close()
+    with open('export.csv', 'a') as e:
+      print(str(row[0]) + ',' + str(row[1]) + ',' + str(row[2]) + ',' , file=e)
+      e.close()
+  f.close()
