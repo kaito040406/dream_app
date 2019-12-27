a3aad65269da971370f00c9e6b9c344d3dd2b55d
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-12-27 16:54:53 +0900

    修正

---
+++ python_file/make_seed.py
@@ -0,0 +1,43 @@
+import csv
+import pprint
+import time
+import re
+
+i = 0
+
+with open('output.php', 'a') as e:
+  print('<?php', file=e)
+  print('', file=e)
+  print('use Illuminate\Database\Seeder;', file=e)
+  print('', file=e)
+  print('class UranaisTableSeeder extends Seeder', file=e)
+  print('{', file=e)
+  print('   /**', file=e)
+  print('   * Run the database seeds.', file=e)
+  print('   *', file=e)
+  print('   * @return void', file=e)
+  print('   */', file=e)
+  print('   {', file=e)
+  print("       DB::table('uranais')->insert([", file=e)
+  e.close()
+
+with open('export5.csv') as f:
+  reader = csv.reader(f)
+  for row in reader:
+    if i != 0:
+      with open('output.php', 'a') as e:
+        print("         [", file=e)
+        print("           'title' => " + "'" + row[0] + "',", file=e)
+        print("           'text' => " + "'" + row[1] + "',", file=e)
+        print("           'level' => " + "'" + row[2] + "',", file=e)
+        print("         ],", file=e)
+        e.close()
+    i = i + 1
+  f.close()
+
+
+with open('output.php', 'a') as e:
+  print("       ]);", file=e)
+  print('   }', file=e)
+  print('}', file=e)
+  e.close()
